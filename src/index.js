61f2224d58adc45f31313a8a41eddc014efa953a
zeljkaivanisevic <57908220+zeljkaivanisevic@users.noreply.github.com>
2020-04-11 13:27:15 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import storage from './memory_storage.js'
 import cors from 'cors'
 import connect from './db.js'
-const fetch = require('node-fetch')
+
 
 const app = express()  // instanciranje aplikacije
 const port = 3000  // port na kojem će web server slušati
@@ -13,45 +13,25 @@ app.use(express.json()) // automatski dekodiraj JSON poruke
 /* čitanje postova iz MongoDB */
 /* čitanje postova iz MongoDB uz pretragu i filtriranje */
 
-    
-    async function go() {
-        console.log("funkcija go")
-        console.time("dohvat 1")
-        await dohvati("http://localhost:3000/posts")
-        console.timeEnd("dohvat 1")
-       
-        console.time("dohvat 2")
-        await dohvati("http://localhost:3000/posts?test")
-        console.timeEnd("dohvat 2")
-       
-        console.time("dohvat 3")
-        await dohvati("http://localhost:3000/posts")
-        console.timeEnd("dohvat 3")
-       }
-      
-       go ()
-    
-     function dohvati(url){
-        let q=fetch(url).then(a =>{return a.json()})
-        return q
-        }
-    
-    app.get('/posts', async (req, res) => {
-        
-      
-        let db = await connect()
-        let cursor = await db.collection("posts").find()
-        let results = await cursor.toArray()
-       
-        results.forEach(e => {
-            e.id = e._id
-            delete e._id
-        })
-    
-        res.json(results)
-    })
-    
 
+
+
+app.get('/posts', async (req, res) => {
+    console.log(req.query);
+    var d = new Date();
+    let last6= d.setMonth(d.getMonth() - 6) //last 6 months
+    
+    let db = await connect()
+    let cursor = await db.collection("posts").find({ postedAt: { $gte: last6.toString() } }).sort({postedAt: -1})
+    let results = await cursor.toArray()
+
+    results.forEach(e => {
+        e.id = e._id
+        delete e._id
+    })
+
+    res.json(results)
+})
 
 
 
